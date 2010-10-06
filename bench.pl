5e558b7f755858dd583a517813a17303b8838761
punytan <punytan@gmail.com>
2010-10-06 20:04:19 +0900

    added: how long does render() take?

--- bench.pl
+++ bench.pl
@@ -8,6 +8,7 @@ use File::stat;
 use File::Spec;
 use File::Slurp;
 use Benchmark qw/cmpthese/;
+use Time::HiRes qw/gettimeofday time tv_interval/;
 
 use Text::Xslate;
 
@@ -17,13 +18,29 @@ my $xslate = Text::Xslate->new(
     path      => ['.'],
 );
 
-print "perl version: $]\n";
+{ # cache !
+    $xslate->render('tiny.tx', {bench => time});
+    $xslate->render('large.tx', {bench => time});
+}
 
 my $t = stat('tiny.tx');
 my $l = stat('large.tx');
 
-printf "size of tiny.tx: %s\nsize of large.tx: %s\n",
-    $t->size, $l->size;
+printf "perl version: %s(xslate: %s)\nsize of tiny.tx: %s\nsize of large.tx: %s\n",
+    $], $Text::Xslate::VERSION, $t->size, $l->size;
+
+{ # how long does render() take?
+    my $tiny_t = [gettimeofday];
+    $xslate->render('tiny.tx', {bench => time});
+    my $tiny_e = tv_interval $tiny_t;
+
+    my $large_t = [gettimeofday];
+    $xslate->render('large.tx', {bench => time});
+    my $large_e = tv_interval $large_t;
+
+    printf "render  tiny.tx: %f\nrender large.tx: %f\n",
+        $tiny_e, $large_e;
+}
 
 sub tiny {
     $xslate->render('tiny.tx', {bench => time});
